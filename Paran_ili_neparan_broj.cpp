55b47bef40e5fb860750c3ad07e6361149ac47a3
Tula99 <39736298+Tula99@users.noreply.github.com>
2018-05-30 15:55:20 +0200

    Create Paran_ili_neparan_broj.cpp

---
+++ Paran_ili_neparan_broj.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int r;
+    cin>>r;
+
+    r & 1 ? cout<<"Broj je paran" : cout<<"Broj je neparan";
+    return 0;
+}
