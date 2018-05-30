621583e2ade983e8da8dd6de5b56634a94a97530
Tula99 <39736298+Tula99@users.noreply.github.com>
2018-05-30 15:37:57 +0200

    Create Mijenjanje_vrijednosti_dva_broja_2.cpp

---
+++ Mijenjanje_vrijednosti_dva_broja_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+
+    a+=b;
+    b=a-b;
+    a-=b;
+
+    cout<<a<<" "<<b<<endl;
+    return 0;
+}
