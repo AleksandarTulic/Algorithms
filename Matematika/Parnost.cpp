2cbbe25fb662de7b247d15c568542d011e34fb03
Aleksandar <39736298+Tula99@users.noreply.github.com>
2018-10-02 19:10:12 +0200

    Create Parnost.cpp

---
+++ Matematika/Parnost.cpp
@@ -0,0 +1,10 @@
+void parnost(int n)
+{
+    n & 1 ? cout<<"Neparan"<<endl : cout<<"paran"<<endl;
+}
+
+void parnost(int n)
+{
+    if ( n % 2 == 0 ) cout<<"Paran"<endl;
+    else cout<<"Neparan"<<endl;
+}
