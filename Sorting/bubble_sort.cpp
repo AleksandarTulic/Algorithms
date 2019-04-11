e6cb59589600d873d6d0339f78380d8f2bbd16dc
Aleksandar <39736298+Tula99@users.noreply.github.com>
2019-04-11 18:34:30 +0200

    Update bubble_sort.cpp

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -1,9 +1,7 @@
-void bubble_sort(int n, int a[])
+void bubble_sort(int a[], int n)
 {
-    int br = n;
-    while (br--)
-        for (int i=0;i<n-1;i++)
-            if ( a[i] > a[i+1] ) swap(a[i],a[i+1]);
-
-    for (int i=0;i<n;i++) cout<<a[i]<<" ";
+    int i,j;
+    for (i=n-1;i>0;i--)
+        for (j=0;j<i;j++)
+            if ( a[j] > a[j+1] ) a[j]^=a[j+1],a[j+1]^=a[j],a[j]^=a[j+1];
 }
