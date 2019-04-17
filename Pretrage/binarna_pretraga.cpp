7f0c9d4c335b5de12aafc8a7ab0c1c2849977a3c
Aleksandar <39736298+Tula99@users.noreply.github.com>
2019-04-17 20:23:31 +0200

    Update binarna_pretraga.cpp

--- Pretrage/binarna_pretraga.cpp
+++ Pretrage/binarna_pretraga.cpp
@@ -23,3 +23,17 @@ int binarna_pretraga2(int a[], int begin, int end, int kljuc)
         else return binarna_pretraga(a, sredina+1, end,kljuc);
     }
 }
+
+int binarna_pretraga3(int a[], int begin, int end, int kljuc)
+{
+    if ( begin <= end )
+    {
+        int imenilac = a[end] - a[begin];
+        int brojilac = (end-begin)*(kljuc-a[begin]);
+        int sredina = begin + brojilac/imenilac;
+
+        if ( a[sredina] == kljuc ) return sredina;
+        else if ( a[sredina] > kljuc ) return binarna_pretraga(a,begin, sredina -1, kljuc);
+        else return binarna_pretraga(a, sredina+1, end,kljuc);
+    }
+}
