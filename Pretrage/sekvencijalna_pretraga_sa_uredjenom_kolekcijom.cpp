548d9de39ec046b5031ac0bce7ab40b10f16347c
Aleksandar <39736298+Tula99@users.noreply.github.com>
2019-07-22 22:01:28 +0200

    Delete sekvencijalna_pretraga_sa_uredjenom_kolekcijom.cpp

--- Pretrage/sekvencijalna_pretraga_sa_uredjenom_kolekcijom.cpp
+++
@@ -1,8 +0,0 @@
-int Seq_pretraga_sa_uredjenom_kolekcijom(int a[], int n, int kljuc)
-{
-    int br = 0;
-    while ( a[br] < kljuc && br < n ) br++;
-
-    if ( a[br] == kljuc ) return br;
-    return -1;
-}
